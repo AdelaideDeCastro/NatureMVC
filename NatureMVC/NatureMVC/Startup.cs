e7c26df007f1505780034fc46a8cb36215155c30
AdeBia <decastroadelaide@gmail.com>
2017-09-18 16:11:28 +0200

    start

---
+++ NatureMVC/NatureMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NatureMVC.Startup))]
+namespace NatureMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
