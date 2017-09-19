f6e19770340444c07cced372b2b01312f1cedbc6
AdeBia <decastroadelaide@gmail.com>
2017-09-19 21:14:42 +0200

    ghh

--- NatureMVC/NatureMVC/App_Start/BundleConfig.cs
+++ NatureMVC/NatureMVC/App_Start/BundleConfig.cs
@@ -11,8 +11,6 @@ namespace NatureMVC
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                  "~/Content/site.css",
